1417cc431382cfdf95312543f2c567e84b43c5e8
Ron Sala <ron.sala@gmail.com>
2018-07-15 01:28:32 +0000

    automatically backed up by learn

--- models/item.rb
+++ models/item.rb
@@ -2,9 +2,10 @@ class Item
   attr_reader :item
 
   def initialize(params)
+    binding.pry
+
     @item = params[name] #passes test but breaks in browser
         # @item = params[name]
-    binding.pry
   end
 
 end
